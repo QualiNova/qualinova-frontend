8c12859bda53573e66c44e3e1c5f282711f97a37
JamesVictor-o <victorjames40v@gmail.com>
2025-03-22 23:41:06 +0100

    implemeted the required changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,11 @@
-import HeroSection from "@/components/mainPageComponents/HeroSection"
+import HeroSection from "@/components/organisms/HeroSection/HeroSection";
 const Home = () => {
   return (
     <div>
-<<<<<<< HEAD
       <HeroSection/>
     </div>
-  )
-=======
-      <>Base Page</>
-    </div>
   );
->>>>>>> 584d957b0cfe54d84a4a1ed00f0d7b498beefaac
+
 }
 
 export default Home
