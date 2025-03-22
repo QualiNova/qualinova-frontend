f30089164d9bfbfec0c73e07d67952f0a7576507
James_Victor <112592627+JamesVictor-O@users.noreply.github.com>
2025-03-23 00:18:27 +0100

    Feature: Main Page (first section) (#11)
    * feat: implement Main Page (first section) with responsive design and Atomic Design structure
    
    * feat: implement Main Page (first section) with responsive design and Atomic Design structure
    
    * implemeted the required changes
    
    ---------
    
    Co-authored-by: Luis Eduardo Rojas Carrillo <62352363+titito2005@users.noreply.github.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import KeyFeatures from "@/components/organisms/Keyfeatures/KeyFeatures";
+import HeroSection from "@/components/organisms/HeroSection/HeroSection";
 const Home = () => {
   return (
     <div>
+      <HeroSection/>
       <KeyFeatures/>
     </div>
   );
-
 }
 
 export default Home
