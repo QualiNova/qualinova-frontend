9ff20e0e872e889edabe7c1ec68869af6aedd3bf
bigchills <yemi7164@gmail.com>
2025-03-22 03:25:57 +0100

    chore: cleanup files

--- src/components/pages/register/page.tsx
+++ src/components/pages/register/page.tsx
@@ -1,10 +1,5 @@
-import RegisterForm from "@/components/organisms/Registerform/RegisterForm";
 import React from "react";
 
 export default function page() {
-  return (
-    <div>
-      <RegisterForm />
-    </div>
-  );
+  return <div>base page</div>;
 }
