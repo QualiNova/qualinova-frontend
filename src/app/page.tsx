aa12ccb7778f09715a384f96b44873fb2d0a1cc0
JamesVictor-o <victorjames40v@gmail.com>
2025-03-24 03:24:18 +0100

    ...

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,14 @@ import KeyFeatures from "@/components/mainPageComponents/KeyFeatures"
 const Home = () => {
   return (
     <div>
-<<<<<<< HEAD
+
       <KeyFeatures/>
-    </div>
-  )
-=======
+
+
       <>Base Page</>
     </div>
   );
->>>>>>> 584d957b0cfe54d84a4a1ed00f0d7b498beefaac
+
 }
 
 export default Home
