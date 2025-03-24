a9ed781acb7aca274fdb65dc255de6d08d8c57f4
James_Victor <112592627+JamesVictor-O@users.noreply.github.com>
2025-03-25 00:47:02 +0100

    Feat:implement third section following Figma design (#21)
    * feat: implement Main Page (first section) with responsive design and Atomic Design structure
    
    * feat: implement Main Page (first section) with responsive design and Atomic Design structure
    
    * feat: implement Main Page (second section) with responsive design and Atomic Design structure
    
    - Added the second section of the Main Page following the Figma design specifications
    - Created a reusable  component as part of the Atomic Design structure:
      - Atoms: Typography, buttons, icons
      - Molecules: Card content, interactive elements
      - Organisms: Card layout, section container
    - Ensured responsiveness for all screen sizes (mobile, tablet, desktop)
    - Styled interactive elements (e.g., buttons, hover states) for consistency
    - Added unit tests for the  component and section layout
    - Verified design alignment with Figma mockups
    - Improved accessibility with semantic HTML and ARIA attributes
    
    * ...

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import KeyFeatures from "@/components/organisms/Keyfeatures/KeyFeatures";
 import HeroSection from "@/components/organisms/HeroSection/HeroSection";
+import HowItWorks from "@/components/organisms/HowItWorks/HowItWorks";
 const Home = () => {
   return (
     <div>
       <HeroSection/>
       <KeyFeatures/>
+       <HowItWorks/>
     </div>
   );
-}
 
+}
 export default Home
 
